db74f6b2e129f9549c5411ff3b87a0c0d27a47dc
4b2g0039@office.stust.edu.tw <4b2g0039@office.stust.edu.tw>
2024-12-24 16:45:12 +0800

    調整 MainWindow 佈局並新增 AQI 抓取功能
    將 MainWindow 的高度從 450 改為 1000，寬度從 800 改為 1200。
    在 Grid 中新增了 RowDefinitions 和 ColumnDefinitions，分別定義了三行和三列的佈局。
    在 Grid 的第一行新增了一個 StackPanel，內含一個 Label 和一個水平排列的 StackPanel，該 StackPanel 包含一個 TextBox 和一個 Button。
    在 Grid 的第二行新增了一個 TextBox 和一個 GridSplitter。
    在 Grid 的第二行第三列新增了一個空的 Grid。
    在 Grid 的第三行新增了一個 StatusBar，內含一個 StatusBarItem 和一個 TextBlock。
    在 MainWindow.xaml.cs 中移除了多個 using 指令，只保留了 using System.Windows。
    在 MainWindow.xaml.cs 中新增了一個 defaultURL 字串，並在 MainWindow 的建構子中將 UrlTextBox 的文字設為 defaultURL。
    在 MainWindow.xaml.cs 中新增了一個非同步方法 GetAQIButton_Click，當按下 GetAQIButton 時，會將 ContentTextBox 的文字設為 "抓取資料中..."。
    
    更新 MainWindow 佈局並新增功能
    
    修改了 MainWindow.xaml 的 Title 屬性，將視窗的高度從 450 改為 1000，寬度從 800 改為 1200。新增了 Grid 的 RowDefinitions 和 ColumnDefinitions，分別定義了三行和三列的佈局。在 Grid 的第一行和第一列新增了一個 StackPanel，內含一個 Label 和一個 StackPanel，後者包含一個 TextBox 和一個 Button。在 Grid 的第二行和第一列新增了一個 TextBox，用於顯示內容。在 Grid 的第二行和第二列新增了一個 GridSplitter，用於調整列寬。在 Grid 的第二行和第三列新增了一個空的 Grid。在 Grid 的第三行新增了一個 StatusBar，內含一個 StatusBarItem 和一個 TextBlock，用於顯示狀態訊息。
    
    在 MainWindow.xaml.cs 中移除了多個不必要的 using 指令。在 MainWindow 類別中新增了一個 defaultURL 字串，並在建構函式中將其設為 UrlTextBox 的預設值。新增了一個 GetAQIButton_Click 非同步方法，當按下按鈕時，會在 ContentTextBox 中顯示 "抓取資料中..." 的訊息。

--- WPF_AQI/MainWindow.xaml.cs
+++ WPF_AQI/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WPF_AQI
 {
@@ -16,9 +7,16 @@ namespace WPF_AQI
     /// </summary>
     public partial class MainWindow : Window
     {
+        string defaultURL = "https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON";
         public MainWindow()
         {
             InitializeComponent();
+            UrlTextBox.Text = defaultURL;
+        }
+
+        private async void GetAQIButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContentTextBox.Text = "抓取資料中...";
         }
     }
 }
