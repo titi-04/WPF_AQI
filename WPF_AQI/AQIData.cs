d78c3f75ccfacfb24866bcf68bd1ea800b8d7ae9
4b2g0039@office.stust.edu.tw <4b2g0039@office.stust.edu.tw>
2025-01-04 15:11:31 +0800

    新增 LiveCharts.Wpf 支援及 AQI 資料結構
    在 `MainWindow.xaml` 中新增了 `LiveCharts.Wpf` 的命名空間引用，並且在介面中新增了幾個 `Border` 和 `GridSplitter` 元件。其中一個 `Border` 包含了一個 `DataGrid`，另一個 `Border` 包含了一個 `WrapPanel`，最後一個 `Border` 包含了一個 `CartesianChart`，用於顯示 AQI 資料。
    
    在 `WPF_AQI.csproj` 中新增了 `LiveCharts.Wpf` 套件的引用，版本為 `0.9.7`。
    
    在 `AQIData.cs` 中新增了一些類別來表示 AQI 資料的結構，包括 `Extras`、`Field`、`Info`、`Links`、`Record` 和 `AQIData` 類別，這些類別包含了各種屬性來儲存 AQI 資料。

---
+++ WPF_AQI/AQIData.cs
@@ -0,0 +1,69 @@
+﻿namespace WPF_AQI
+{
+    public class Extras
+    {
+        public string api_key { get; set; }
+    }
+
+    public class Field
+    {
+        public string id { get; set; }
+        public string type { get; set; }
+        public Info info { get; set; }
+    }
+
+    public class Info
+    {
+        public string label { get; set; }
+    }
+
+    public class Links
+    {
+        public string start { get; set; }
+        public string next { get; set; }
+    }
+
+    public class Record
+    {
+        public string sitename { get; set; }
+        public string county { get; set; }
+        public string aqi { get; set; }
+        public string pollutant { get; set; }
+        public string status { get; set; }
+        public string so2 { get; set; }
+        public string co { get; set; }
+        public string o3 { get; set; }
+        public string o3_8hr { get; set; }
+        public string pm10 { get; set; }
+
+        public string pm25 { get; set; }
+        public string no2 { get; set; }
+        public string nox { get; set; }
+        public string no { get; set; }
+        public string wind_speed { get; set; }
+        public string wind_direc { get; set; }
+        public string publishtime { get; set; }
+        public string co_8hr { get; set; }
+
+        public string pm25_avg { get; set; }
+        public string pm10_avg { get; set; }
+        public string so2_avg { get; set; }
+        public string longitude { get; set; }
+        public string latitude { get; set; }
+        public string siteid { get; set; }
+    }
+
+    public class AQIData
+    {
+        public List<Field> fields { get; set; }
+        public string resource_id { get; set; }
+        public Extras __extras { get; set; }
+        public bool include_total { get; set; }
+        public string total { get; set; }
+        public string resource_format { get; set; }
+        public string limit { get; set; }
+        public string offset { get; set; }
+        public Links _links { get; set; }
+        public List<Record> records { get; set; }
+    }
+}
